cb4a867282c5198fe5b103ed868841f6c37b3122
GitHub Action <action@github.com>
2024-11-03 13:45:02 +0000

    Bump version to 2.11.1 (211001)

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,8 +40,8 @@ subprojects {
             minSdk = 21
             targetSdk = 31
 
-            versionName = "2.11.2"
-            versionCode = 211002
+            versionName = "2.11.1"
+            versionCode = 211001
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
